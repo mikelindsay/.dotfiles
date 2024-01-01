7d8063938fd445fe573e878ac18583b88ae38dce
mikelindsay <4461292+mikelindsay@users.noreply.github.com>
2024-01-01 09:23:47 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 echo "Building environment"
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt-get --yes --allow install i3
-sudo apt-get --yes --allow install stow
-sudo apt-get --yes --allow install firefox
-sudo apt-get --yes --allow install git
-sudo apt-get --yes --allow install neovim
+sudo apt-get --allow install i3
+sudo apt-get --allow install stow
+sudo apt-get --allow install firefox
+sudo apt-get --allow install git
+sudo apt-get --allow install neovim
 
 URL=git@github.com:mikelindsay/.dotfiles.git
 FOLDER=~/.dotfiles
