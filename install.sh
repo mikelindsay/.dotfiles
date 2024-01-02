232cdfb845e3b4021eeb9c64c05d7f6f401908a2
Michael Lindsay <4461292+mikelindsay@users.noreply.github.com>
2024-01-02 12:41:38 -0500

    added vim plugin install

--- install.sh
+++ install.sh
@@ -113,4 +113,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+vim +PlugInstall +qall
 nvim +PlugInstall +qall
