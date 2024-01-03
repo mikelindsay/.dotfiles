6c8aa2d4ff897bfe37ddea62e53e37ec28a2ad47
Michael Lindsay <4461292+mikelindsay@users.noreply.github.com>
2024-01-03 15:32:29 -0500

    added ghost text and more copilot

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -39,11 +39,21 @@ require("copilot").setup({
   panel = { enabled = false },
 })
 
---    require("copilot_cmp").setup()
+    require("copilot_cmp").setup(
+    { event = { "InsertEnter", "LspAttach" },
+                fix_pairs = true,
+	})
 -- Set up nvim-cmp.
   local cmp = require'cmp'
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+end
 
+  
   cmp.setup({
+experimental = { ghost_text = true, },
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
@@ -54,8 +64,8 @@ require("copilot").setup({
       end,
     },
     window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
+       completion = cmp.config.window.bordered(),
+       documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -63,7 +73,14 @@ require("copilot").setup({
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
+          ["<Tab>"] = vim.schedule_wrap(function(fallback)
+      if cmp.visible() and has_words_before() then
+        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+      else
+        fallback()
+      end
+    end),
+   }),
     sources = cmp.config.sources({
     { name = 'copilot' },
     { name = 'nvim_lsp' },
