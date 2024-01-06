56227d53a2c0ef3cc9db05fac3716026c737e0f3
Michael Lindsay <4461292+mikelindsay@users.noreply.github.com>
2024-01-06 09:29:01 -0500

    typo

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -34,7 +34,6 @@ require("lualine").setup(
 
 local lspconfig = require("lspconfig")
 local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-- )
 -- Set up nvim-cmp.
 local cmp = require "cmp"
 local has_words_before = function()
