7078b4897181d100dbccd9a8a4c783b3d8fa1fc8
Michael Lindsay <4461292+mikelindsay@users.noreply.github.com>
2024-01-06 08:55:03 -0500

    hotkeys

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -3,77 +3,23 @@ local action_state = require("telescope.actions.state")
 local harpoon = require("harpoon")
 harpoon:setup({})
 
-vim.keymap.set(
-    "n",
-    "<leader>a",
-    function()
-        harpoon:list():append()
-    end
-)
-vim.keymap.set(
-    "n",
-    "<C-e>",
-    function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-    end
-)
+vim.keymap.set( "n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set( "n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set(
-    "n",
-    "<M-6>",
-    function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-    end
-)
+vim.keymap.set( "n", "<M-6>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set(
-    "n",
-    "<M-0>",
-    function()
-        harpoon:list():select(1)
-    end
-)
-vim.keymap.set(
-    "n",
-    "<M-9>",
-    function()
-        harpoon:list():select(2)
-    end
-)
-vim.keymap.set(
-    "n",
-    "<M-8>",
-    function()
-        harpoon:list():select(3)
-    end
-)
-vim.keymap.set(
-    "n",
-    "<M-7>",
-    function()
-        harpoon:list():select(4)
-    end
-)
+vim.keymap.set( "n", "<M-0>", function() harpoon:list():select(1) end)
+vim.keymap.set( "n", "<M-9>", function() harpoon:list():select(2) end)
+vim.keymap.set( "n", "<M-8>", function() harpoon:list():select(3) end)
+vim.keymap.set( "n", "<M-7>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set(
-    "n",
-    "<C-S-P>",
-    function()
-        harpoon:list():prev()
-    end
-)
-vim.keymap.set(
-    "n",
-    "<C-S-N>",
-    function()
-        harpoon:list():next()
-    end
-)
+vim.keymap.set( "n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set( "n", "<C-S-N>", function() harpoon:list():next() end)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 require("mason").setup()
+
 
 require("mason-lspconfig").setup {
     ensure_installed = {"lua_ls", "powershell_es"}
@@ -232,61 +178,4 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
         update_in_insert = true
     }
 )
----basic telescope configuration
----al conf = require("telescope.config").values
----al function toggle_telescope(harpoon_files)
---- local file_paths = {}
---- for _, item in ipairs(harpoon_files.items) do
----     table.insert(file_paths, item.value)
---- end
-
---- require("telescope.pickers").new({}, {
----     prompt_title = "Harpoon",
----     finder = require("telescope.finders").new_table({
----         results = file_paths,
----     }),
----     previewer = conf.file_previewer({}),
----     sorter = conf.generic_sorter({}),
----     -- Make telescope select buffer from harpoon list
----     attach_mappings = function(_, map)
----         local function list_find(list, func)
----             for i, v in ipairs(list) do
----                 if func(v, i, list) then
----                     return i, v
----                 end
----             end
----         end
-
----         actions.select_default:replace(function(prompt_bufnr)
----             local curr_picker = action_state.get_current_picker(prompt_bufnr)
----             local curr_entry = action_state.get_selected_entry()
----             if not curr_entry then
----                 return
----             end
----             actions.close(prompt_bufnr)
-
----             local idx, _ = list_find(curr_picker.finder.results, function(v)
----                 if curr_entry.value == v.value then
----                     return true
----                 end
----                 return false
----             end)
----             harpoon:list():select(idx)
----         end)
----         -- Delete entries from harpoon list with <C-d>
----         map({ 'n', 'i' }, '<C-d>', function(prompt_bufnr)
----             local current_picker = action_state.get_current_picker(prompt_bufnr)
-
----             current_picker:delete_selection(function(selection)
----                 harpoon:list():removeAt(selection.index)
----             end)
----         end
----         )
----         return true
----     end
---- }):find()
----nd
-
----im.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
---    { desc = "Open harpoon window" })
 
