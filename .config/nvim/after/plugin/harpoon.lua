717fdbcdcb9d91b522c909a175006773fa27c85c
Michael Lindsay <4461292+mikelindsay@users.noreply.github.com>
2024-01-06 22:08:44 -0500

    added as submodule

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -3,9 +3,7 @@ harpoon:setup({})
 
 vim.keymap.set( "n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set( "n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
 vim.keymap.set( "n", "<M-6>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
 vim.keymap.set( "n", "<M-0>", function() harpoon:list():select(1) end)
 vim.keymap.set( "n", "<M-9>", function() harpoon:list():select(2) end)
 vim.keymap.set( "n", "<M-8>", function() harpoon:list():select(3) end)
