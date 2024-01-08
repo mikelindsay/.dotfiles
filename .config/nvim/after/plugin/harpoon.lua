6f8a20844bb6a6ee99c08a062b461de543b36117
Michael Lindsay <4461292+mikelindsay@users.noreply.github.com>
2024-01-07 23:18:47 -0500

    removed plugin

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -8,6 +8,7 @@ harpoon:setup({
 		    end,
 		},
 })
+
 vim.keymap.set( "n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set( "n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 vim.keymap.set( "n", "<M-6>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
